8ff1393c1fab9e3301d18c3151460ee2524dd8ea
Vaibhav Kadwe <vkkadwe@gmail.com>
2026-02-10 10:24:28 +0530

    Change X and Y types to long long for larger values
    Changed variable types of X and Y from int to long long to accommodate larger multiplication results.

--- C.cpp
+++ C.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int X,Y;
+    long long X,Y; //NOTE-Here we are using long long since the limit of X and Y is till 10^5 Then thier Multiplucation can reach up to 10^10.
     cin>>X>>Y;
     cout<<X<<" + "<<Y<<" = "<<X+Y<<endl;
     cout<<X<<" * "<<Y<<" = "<<X*Y<<endl;
